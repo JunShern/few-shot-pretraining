fa64ae00832167a7f63ec2996b57d0107cb0d67e
Chan Jun Shern <chanjunshern@gmail.com>
2021-11-12 02:00:30 +0800

    mkdir -p

--- dataset/scripts/download_pile.sh
+++ dataset/scripts/download_pile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir ./base_datasets/pile
+mkdir -p ./base_datasets/pile
 pushd ./base_datasets/pile
 # wget -m -np -c -U "eye02" -w 2 -R "index.html*" "https://the-eye.eu/public/AI/pile/"
 wget -m -np -c -U "eye02" -w 2 "https://the-eye.eu/public/AI/pile/val.jsonl.zst" --no-check-certificate
