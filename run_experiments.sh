c7a2e1ebb4bf601b65886420c3cdc07b721328fa
Chan Jun Shern <chanjunshern@gmail.com>
2021-11-23 12:50:52 -0500

    Foray into slurm with first_batch_train

--- run_experiments.sh
+++ run_experiments.sh
@@ -27,17 +27,18 @@
 # python finetune_with_dataset.py -d configs/dataset/QuestionAnswerStringsV2Criterion_1k.yaml -f configs/finetune/default.json -m EleutherAI/gpt-j-6B
 
 
-# python finetune_with_dataset.py -d configs/dataset/QuestionAnswerStringsV2Criterion_1k.yaml -f configs/finetune/default.json -m gpt2-xl
+# first-batch-train
+# python finetune_with_dataset.py -d configs/dataset/AllDocuments_1k.yaml -f configs/finetune/default.json -m gpt2-large
 # python finetune_with_dataset.py -d configs/dataset/QuestionAnswerStringsV2Criterion_1k.yaml -f configs/finetune/default.json -m gpt2-large
-# python finetune_with_dataset.py -d configs/dataset/QuestionAnswerStringsV2Criterion_1k.yaml -f configs/finetune/default.json -m gpt2-medium
-# python finetune_with_dataset.py -d configs/dataset/QuestionAnswerStringsV2Criterion_1k.yaml -f configs/finetune/default.json -m gpt2
-
-# python finetune_with_dataset.py -d configs/dataset/QuestionAnswerStringsV2Criterion_100k.yaml -f configs/finetune/default.json -m gpt2-xl
 # python finetune_with_dataset.py -d configs/dataset/QuestionAnswerStringsV2Criterion_100k.yaml -f configs/finetune/default.json -m gpt2-large
-# python finetune_with_dataset.py -d configs/dataset/QuestionAnswerStringsV2Criterion_100k.yaml -f configs/finetune/default.json -m gpt2-medium
-# python finetune_with_dataset.py -d configs/dataset/QuestionAnswerStringsV2Criterion_100k.yaml -f configs/finetune/default.json -m gpt2
-
-# python finetune_with_dataset.py -d configs/dataset/ExamplesStringsCriterion_100k.yaml -f configs/finetune/default.json -m gpt2-xl
 # python finetune_with_dataset.py -d configs/dataset/ExamplesStringsCriterion_100k.yaml -f configs/finetune/default.json -m gpt2-large
+
+# python finetune_with_dataset.py -d configs/dataset/AllDocuments_1k.yaml -f configs/finetune/default.json -m EleutherAI/gpt-neo-1.3B
+# python finetune_with_dataset.py -d configs/dataset/QuestionAnswerStringsV2Criterion_1k.yaml -f configs/finetune/default.json -m EleutherAI/gpt-neo-1.3B
+# python finetune_with_dataset.py -d configs/dataset/QuestionAnswerStringsV2Criterion_100k.yaml -f configs/finetune/default.json -m EleutherAI/gpt-neo-1.3B
+# python finetune_with_dataset.py -d configs/dataset/ExamplesStringsCriterion_100k.yaml -f configs/finetune/default.json -m EleutherAI/gpt-neo-1.3B
+
+# python finetune_with_dataset.py -d configs/dataset/AllDocuments_1k.yaml -f configs/finetune/default.json -m gpt2-medium
+# python finetune_with_dataset.py -d configs/dataset/QuestionAnswerStringsV2Criterion_1k.yaml -f configs/finetune/default.json -m gpt2-medium
+# python finetune_with_dataset.py -d configs/dataset/QuestionAnswerStringsV2Criterion_100k.yaml -f configs/finetune/default.json -m gpt2-medium
 # python finetune_with_dataset.py -d configs/dataset/ExamplesStringsCriterion_100k.yaml -f configs/finetune/default.json -m gpt2-medium
-# python finetune_with_dataset.py -d configs/dataset/ExamplesStringsCriterion_100k.yaml -f configs/finetune/default.json -m gpt2
