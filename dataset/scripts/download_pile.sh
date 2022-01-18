78d83724e856700af356f7de144d148721ff4038
Chan Jun Shern <chanjunshern@gmail.com>
2022-01-17 21:16:22 -0500

    Download pile script should download the full dataset by default

--- dataset/scripts/download_pile.sh
+++ dataset/scripts/download_pile.sh
@@ -2,6 +2,6 @@
 
 mkdir -p ./base_datasets/pile
 pushd ./base_datasets/pile
-# wget -m -np -c -U "eye02" -w 2 -R "index.html*" "https://the-eye.eu/public/AI/pile/"
-wget -m -np -c -U "eye02" -w 2 "https://the-eye.eu/public/AI/pile/val.jsonl.zst" --no-check-certificate
+wget -m -np -c -U "eye02" -w 2 -R "index.html*" "https://the-eye.eu/public/AI/pile/"
+# wget -m -np -c -U "eye02" -w 2 "https://the-eye.eu/public/AI/pile/val.jsonl.zst" --no-check-certificate
 popd
